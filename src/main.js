c36211cc8dc40c9a2a4d49ebfd650b79d6731a40
wanghan <minglongoc@me.com>
2019-12-11 18:49:23 +0800

    123

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/styles/reset.css'
+// 移动端1px解决方案
+import './assets/styles/border.css'
+// 移动端300ms延迟解决方案 cnpm install fastclick --save
+import fastClick from 'fastclick'
+import './assets/styles/iconfont.css'
+
+Vue.config.productionTip = false
+
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <home-header></home-header>
+      hello
+    </div>
+</template>
+
+<script>
+  import HomeHeader from './components/Header'
+  export default {
+    components: {
+      HomeHeader: HomeHeader
+    }
+  }
+</script>
+
+<style>
+
+</style>
