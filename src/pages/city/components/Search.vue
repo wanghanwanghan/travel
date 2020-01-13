0b6cf60c4b1df933336f7b8375ed69fc9b133046
wanghan <minglongoc@me.com>
2020-01-13 13:15:22 +0800

    321

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,14 +1,62 @@
 <template>
   <div>
     <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或者拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配结果</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
   export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data () {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    watch: {
+      keyword () {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        if (!this.keyword) {
+          this.list = []
+          return
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities) {
+            this.cities[i].forEach((val) => {
+              //可以搜索到，含有
+              if (val.name.indexOf(this.keyword) > -1 || val.spell.indexOf(this.keyword) > -1) {
+                result.push(val)
+              }
+            })
+          }
+          this.list = result
+        },100)
+      }
+    },
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
+    },
+    computed: {
+      hasNoData () {
+        return !this.list.length
+      }
+    }
   }
 </script>
 
@@ -27,6 +75,21 @@
     border-radius .6rem
     color #666
     padding 0 .1rem
+  .search-content
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    background #eee
+    z-index 1
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    background #fff
+    color #666
+
 
 
 
